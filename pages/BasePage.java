b8f89ee11c46ac6698a2ff0f7329eaf2f48285c3
Ale230992 <alexandranicoara92@yahoo.ro>
2023-06-12 21:57:28 +0300

    Added Sign In

--- pages/BasePage.java
+++ pages/BasePage.java
@@ -24,4 +24,14 @@ public class BasePage {
         LOG.info("Close the browser");
         driver.quit();
     }
+
+    public static void sleep (long ms){
+        try{
+            Thread.sleep(ms);
+        } catch (InterruptedException e){
+            e.printStackTrace();
+        }
+    }
+
+
 }
--- tests/SignInTest/SignInTest.java
+++ tests/SignInTest/SignInTest.java
@@ -5,17 +5,56 @@ import org.slf4j.LoggerFactory;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import tests.BaseTest;
+import static pages.BasePage.sleep;
 
 public class SignInTest extends BaseTest {
     private static final Logger LOG = LoggerFactory.getLogger(SignInTest.class);
 
     @Test
-    public void checkIfSignInButtonIsDisplayed(){
+    public void SignIn() {
+        String email = "grupa1@automation.com";
+        String email2 = "grupa1@automation.com";
+        String password = "parola";
+        String password2 = "parola";
 
         LOG.info("Check 'Sign in' button");
         Assert.assertTrue(signInPage.isSignInButtonDisplayed(), "Button is not displayed");
 
+        LOG.info("Click button");
+        signInPage.clickSignInButton();
+        sleep (3000);
+
+        LOG.info("Check Logo");
+        Assert.assertTrue(signInPage.isLogoDisplayed(), "Logo is not displayed");
+        sleep (3000);
+
+        LOG.info("Click Enter and check message");
+        signInPage.clickEnterButton();
+        Assert.assertTrue(signInPage.isErrorMessageDisplayed(), "Error msg is not displayed");
+        sleep (3000);
+
+        LOG.info("Complete the fields");
+        signInPage.typeInSignInEmailField(email);
+        signInPage.typeInSignInPasswordField(password);
+        sleep (3000);
+
+        LOG.info("Click Enter and check message");
+        signInPage.clickEnterButton();
+        Assert.assertTrue(signInPage.isErrorMessageDisplayed(), "Error msg is not displayed");
+        sleep (3000);
+
+        LOG.info("Complete the fields");
+        signInPage.typeInSignInEmailField(email2);
+        signInPage.typeInSignInPasswordField(password2);
+        sleep (3000);
+
+        LOG.info("Click Enter and check message");
+        signInPage.clickEnterButton();
+        Assert.assertTrue(signInPage.isErrorMessageDisplayed(), "Error msg is not displayed");
+        sleep (3000);
+
+
+
+
     }
-
-
 }
